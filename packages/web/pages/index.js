f2e215f3b558efb6934ed6616b738158c6b7e000
Walee Ahmed <walee.tausif@gmail.com>
2018-01-07 13:23:42 -0500

    added linitng and formatting setup using eslint and prettier

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
 const Test = styled.h1`
-  color: blue
+  color: blue;
 `
-const IndexPage = () =>
-  <Test>Hello Konverter</Test>
+
+const IndexPage = () => <Test>Hello Konverter</Test>
 
 export default IndexPage
